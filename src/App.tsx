c4f5f2128d51d3cc1bc13632ff27c46bead73bd8
Klas Björkqvist <klas.bjorkqvist@gmail.com>
2018-10-21 19:39:16 +0200

    Show faults per day of the week.

--- src/App.tsx
+++ src/App.tsx
@@ -28,6 +28,7 @@ interface IStats {
 }
 
 const HourToString = (x: number): string => `${x < 10 ? "0" : ""}${x}:00`;
+const DayToString = (x: number): string => "Monday Tuesday Wednesday Thursday Friday Saturday Sunday".split(" ")[x];
 
 class App extends React.Component {
   public render() {
@@ -43,12 +44,21 @@ class App extends React.Component {
               return <p>Error: {error || "Unknown error"}</p>;
             }
 
+            const faultsOnDay = data.per_day
+              .map(x => ({day: new Date(x.date).getDay(), count: x.count}))
+              .reduce((a, v) => {
+                a[v.day] += v.count;
+                return a;
+              }, [0,0,0,0,0,0,0])
+              .map((x,i) => ({name: DayToString(i), count: x}));
+
             return (<div>
               <p>Status at {new Date(data.today.time).toLocaleString("sv-SE", {timeZoneName: "short"})}</p>
               <div>
                 <WeeksDisplay weekData={data.per_week} />
               </div>
               <PieChart title="Faults per hour" data={data.per_hour.map(x => ({name: HourToString(x.hour), count: x.count }))} />
+              <PieChart title="Faults per day of the week" data={faultsOnDay} />
             </div>);
           }}
         </Fetcher>
